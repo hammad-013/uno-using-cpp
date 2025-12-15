19c571d20ea8701cf6ea7a3967ef0c1a75d4a92d
Momina Umar <m61963193@gmail.com>
2025-12-15 10:09:45 -0800

    Make function to load images

--- uno-queue.cpp
+++ uno-queue.cpp
@@ -1,16 +1,81 @@
 #include "raylib.h"
 
-int main() {
-  const int screenWidth = 800;
-  const int screenHeight = 600;
-  InitWindow(screenWidth, screenHeight, "raylib basic window");
-  SetTargetFPS(60);
-  while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(DARKGREEN);
-    DrawText("PLAYER 1 TURN", 20, 20, 20, RED);
-    EndDrawing();
-  }
-  CloseWindow();
-  return 0;
+int main()
+{
+    InitWindow(800, 600, "UNO Card Image Test");
+    SetTargetFPS(60);
+
+    Texture2D cardTexture = LoadTexture("assets/cards/red_5.png");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(DARKGREEN);
+
+        DrawTexture(cardTexture, 350, 200, WHITE);
+
+        EndDrawing();
+    }
+
+    UnloadTexture(cardTexture);
+    CloseWindow();
+    return 0;
+}
+string getCardImagePath(const Card& c)
+{
+    string path = "assets/cards/";
+
+
+    string color = c.color;
+    for (char &ch : color)
+        ch = tolower(ch);
+
+
+    if (color == "wild")
+    {
+        if (c.value == "+4")
+            return path + "wild_4.png";
+        else
+            return path + "wild_wild.png";
+    }
+
+
+    path += color + "_";
+
+    string value = c.value;
+    for (char &ch : value)
+        ch = tolower(ch);
+
+    path += value + ".png";
+
+    return path;
+}
+string getCardImagePath(const Card& c)
+{
+    string path = "assets/cards/";
+
+
+    string color = c.color;
+    for (char &ch : color)
+        ch = tolower(ch);
+
+
+    if (color == "wild")
+    {
+        if (c.value == "+4")
+            return path + "wild_4.png";
+        else
+            return path + "wild_wild.png";
+    }
+
+    // Normal cards
+    path += color + "_";
+
+    string value = c.value;
+    for (char &ch : value)
+        ch = tolower(ch);
+
+    path += value + ".png";
+
+    return path;
 }
