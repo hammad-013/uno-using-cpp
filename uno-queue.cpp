5469c62a2ad466c293efc226cea9417d5f828fe2
junaid-gondal <gondalj646@gmail.com>
2025-12-21 20:30:47 +0500

    added code

--- uno-queue.cpp
+++ uno-queue.cpp
@@ -149,9 +149,215 @@ bool isValidMove(Card &played, Card &top, string forcedColor)
         }
     return false;
 }
+void playGame()
+ {
+    int totalplayer = 2;
+    queue<Card> deck, discard;
+    queue<Card> players[totalplayer];
+
+    initializeDeck(deck);
+    shuffleDeck(deck);
+    dealInitialCards(players, totalplayer, deck);
+
+    cout << " UNO GAME START "<<endl;
+
+
+    while (true)
+        {
+        Card c = drawCard(deck);
+        if (c.value != "Wild" && c.value != "+4")
+         {
+            discard.push(c);
+            cout << "First discard = " << c.color << " " << c.value << endl;
+            break;
+        } else
+         {
+            deck.push(c);
+            shuffleDeck(deck);
+        }
+    }
+
+    int cur = 0;
+    string forcedColor = "";
+    int direction = 1;
+
+    while (true)
+        {
+
+
+        cout << "Player " << cur + 1 << " turn:" << endl;
+        showPlayerHand(players[cur]);
+
+        Card top = discard.back();
+        if (forcedColor != "")
+        {
+            cout << "Forced color: " << forcedColor << endl;
+        }
+        cout << "Top discard: " << top.color << " " << top.value << endl;
+
+        int handSize = players[cur].size();
+        cout << "Choose card and -1 to draw): ";
+        int choice;
+        cin >> choice;
+        cout<<endl;
+
+
+        if (choice == -1)
+            {
+              if (deck.empty())
+            {
+                cout << "Deck empty, cannot draw"<<endl;
+
+                if(cur==0)
+                {
+                    cur++;
+
+                }
+             else  if(cur==1)
+                {
+                    cur --;
+
+
+                }
+                continue;
+            }
+
+            Card drawn = drawCard(deck);
+
+            players[cur].push(drawn);
+
+            cout << "You drew: " << drawn.color << " " << drawn.value << endl;
+
+            showPlayerHand(players[cur]);
+
+        }
+
+
+        queue<Card> tmp;
+        Card play;
+        for (int i = 0; i < handSize; i++)
+        {
+            Card c = players[cur].front();
+            players[cur].pop();
+            if (i == choice)
+            {
+                play = c;
+
+                }
+            else tmp.push(c);
+        }
+        players[cur] = tmp;
+
+
+        if (!isValidMove(play, top, forcedColor))
+            {
+            cout << "Invalid move. You cannot play this card now."<<endl;
+            players[cur].push(play);
+                if(cur==0)
+                {
+                    cur++;
+
+                }
+               else if(cur==1)
+                {
+                    cur--;
+
+
+                }
+                continue;
+
+        }
+
+        discard.push(play);
+
+        cout << "Player " << cur + 1 << " played: " << play.color << " " << play.value << endl;
+
+        if (play.value == "Wild" || play.value == "+4")
+        {
+
+            forcedColor = chooseWildColor();
+        }
+        else
+           {
+               forcedColor = "";
+           }
 
 
 
+        if(top.value=="Draw2" && top.color==play.color&& play.value=="Draw2")
+        {
+
+        cout << "Invalid move. You cannot placed draw on draw "<<endl;
+
+            players[cur].push(play);
+             if(cur==0)
+            {
+                    cur++;
+
+            }
+          else  if(cur==1)
+            {
+                    cur --;
+
+
+                }
+                continue;
+
+        }
+
+         else if (play.value == "Draw2")
+         {   int next;
+                if(cur==0)
+                {
+                    next=cur+1;
+
+                }
+             else if(cur==1)
+                {
+                    next=cur -1;
+
+                }
+
+
+            cout << "Next player draws 2 cards. and also skipped" << endl;
+            for (int i = 0; i < 2; i++)
+                {
+                    if (deck.empty())
+                    {
+                      break;
+                    }
+                else
+                {
+                    players[next].push(drawCard(deck));
+
+                }
+                }
+            continue;
+        }
+
+       else
+        {  if(cur==0)
+       {
+          cur++;
+
+        }
+      else if(cur==1)
+        {
+          cur --;
+
+
+        }
+
+        }
+    }
+}
+
+
+int main()
+ {
+    playGame();
+    return 0;
+}
 
 
 
