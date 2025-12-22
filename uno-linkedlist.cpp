955f82866efeb9c662715366abe8618becc853f4
Ghostaura-svg <bscs24f01@namal.edu.pk>
2025-12-22 17:58:23 +0500

    make functions gui friendly & test basic gui

--- uno-linkedlist.cpp
+++ uno-linkedlist.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 
+#include "raylib.h"
+
 using namespace std;
 
 mt19937 rng(random_device{}());
@@ -197,32 +199,33 @@ public:
   }
 };
 
-enum Color { RED, GREEN, BLUE, YELLOW, WILD };
+enum CardColor { CARD_RED, CARD_GREEN, CARD_BLUE, CARD_YELLOW, WILD };
 
 enum CardType { NUMBER, SKIP, REVERSE, DRAW_TWO, WILD_COLOR, WILD_DRAW_FOUR };
+enum TurnState { WAITING_FOR_INPUT, TURN_FINISHED, GAME_OVER };
 
 class Card {
 public:
-  Color color;
+  CardColor color;
   CardType type;
   int number; // valid only if card is
 
   Card() : color(WILD), type(WILD_COLOR), number(-1) {}
-  Card(Color c, CardType t, int n = -1) : color(c), type(t), number(n) {}
+  Card(CardColor c, CardType t, int n = -1) : color(c), type(t), number(n) {}
 
   string toString() const {
     string colorStr;
     switch (color) {
-    case RED:
+    case CARD_RED:
       colorStr = "Red";
       break;
-    case GREEN:
+    case CARD_GREEN:
       colorStr = "Green";
       break;
-    case BLUE:
+    case CARD_BLUE:
       colorStr = "Blue";
       break;
-    case YELLOW:
+    case CARD_YELLOW:
       colorStr = "Yellow";
       break;
     case WILD:
@@ -261,8 +264,8 @@ class Deck {
 
 public:
   Deck() {
-    Color colors[] = {RED, GREEN, BLUE, YELLOW};
-    for (Color color : colors) {
+    CardColor colors[] = {CARD_RED, CARD_GREEN, CARD_BLUE, CARD_YELLOW};
+    for (CardColor color : colors) {
       cards.insertEnd(Card(color, NUMBER, 0));
       for (int i = 1; i <= 9; ++i) {
         cards.insertEnd(Card(color, NUMBER, i));
@@ -333,7 +336,7 @@ class DiscardPile {
 public:
   void addCard(Card c) { pile.insertEnd(c); }
 
-  Card getTopCard() {
+  Card getTopCard() const {
     Node<Card> *tailNode = pile.getTail();
     if (tailNode == nullptr) {
       cout << "Discard pile is empty" << endl;
@@ -389,7 +392,7 @@ public:
     return count;
   }
 
-  bool hasColor(Color color) const {
+  bool hasColor(CardColor color) const {
 
     Node<Card> *temp = hand.getHead();
     if (!temp)
@@ -404,7 +407,7 @@ public:
     return false;
   }
 
-  bool hasPlayable(Card topCard, Color currentColor) {
+  bool hasPlayable(Card topCard, CardColor currentColor) {
     Node<Card> *temp = hand.getHead();
     if (!temp)
       return false;
@@ -457,13 +460,16 @@ public:
 
 class Game {
   bool isGameOver;
-  Color currentColor;
+  CardColor currentColor;
   Deck deck;
   DiscardPile discardPile;
   LinkedList<Player> players;
   int totalPlayers;
   Node<Player> *currentPlayer;
   int direction; // 1 = clockwise, -1 = counter-clockwise
+  TurnState turnState = WAITING_FOR_INPUT;
+  bool waitingForWildColor = false;
+  CardType pendingWildType;
 
 public:
   Game() : isGameOver(false), totalPlayers(4), direction(1) {
@@ -476,7 +482,7 @@ public:
 
   void start() {
     deck.shuffle();
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < 7; i++) {
       Node<Player> *temp = players.getHead();
       for (int j = 0; j < totalPlayers; j++) {
         temp->getData().addToHand(deck.drawCard());
@@ -526,6 +532,16 @@ public:
 
   void reverseDirection() { direction *= -1; }
 
+  Player &getCurrentPlayer() { return currentPlayer->getData(); }
+
+  Card getTopCard() const { return discardPile.getTopCard(); }
+
+  CardColor getCurrentColor() const { return currentColor; }
+
+  void setCurrentColor(CardColor color) { currentColor = color; }
+
+  int getCurrentPlayerId() const { return currentPlayer->getData().getId(); }
+
   bool isCardValid(Card selectedCard) {
     if (selectedCard.color == WILD || selectedCard.type == WILD_COLOR ||
         selectedCard.type == WILD_DRAW_FOUR ||
@@ -552,263 +568,307 @@ public:
   }
 
   bool playSpecialCard(Card playedCard) {
-    currentColor = playedCard.color == WILD ? currentColor : playedCard.color;
+    if (playedCard.color != WILD) {
+      currentColor = playedCard.color;
+    }
+
     if (playedCard.type == REVERSE) {
-      cout << "Direction reversed!" << endl;
       reverseDirection();
       return false;
-    } else if (playedCard.type == SKIP) {
-      Node<Player> *skipped = (direction == 1) ? currentPlayer->getNext()
-                                               : currentPlayer->getPrev();
+    }
 
-      cout << skipped->getData().getName() << " is skipped!" << endl;
-
-      skipNextPlayer();
-
-      return true;
-    } else if (playedCard.type == DRAW_TWO) {
-      Node<Player> *target = (direction == 1) ? currentPlayer->getNext()
-                                              : currentPlayer->getPrev();
-
-      Player &nextPlayer = target->getData();
-
-      cout << nextPlayer.getName() << " draws 2 cards and is skipped!" << endl;
-      nextPlayer.addToHand(safeDraw());
-      nextPlayer.addToHand(safeDraw());
-      skipNextPlayer();
-
-      return true;
-    } else if (playedCard.type == WILD_COLOR) {
-      int colorChoice;
-      cout << "Choose a color (0-Red, 1-Green, 2-Blue, 3-Yellow): ";
-      cin >> colorChoice;
-      switch (colorChoice) {
-      case 0:
-        currentColor = RED;
-        break;
-      case 1:
-        currentColor = GREEN;
-        break;
-      case 2:
-        currentColor = BLUE;
-        break;
-      case 3:
-        currentColor = YELLOW;
-        break;
-      default:
-        cout << "Invalid color choice. Keeping previous color." << endl;
-        break;
-      }
-      cout << "Color changed to "
-           << (currentColor == RED     ? "Red"
-               : currentColor == GREEN ? "Green"
-               : currentColor == BLUE  ? "Blue"
-                                       : "Yellow")
-           << "!" << endl;
-      return false;
-    } else if (playedCard.type == WILD_DRAW_FOUR) {
-      int colorChoice;
-      cout << "Choose a color (0-Red, 1-Green, 2-Blue, 3-Yellow): ";
-      cin >> colorChoice;
-      switch (colorChoice) {
-      case 0:
-        currentColor = RED;
-        break;
-      case 1:
-        currentColor = GREEN;
-        break;
-      case 2:
-        currentColor = BLUE;
-        break;
-      case 3:
-        currentColor = YELLOW;
-        break;
-      default:
-        cout << "Invalid color choice. Keeping previous color." << endl;
-        break;
-      }
-      Node<Player> *target = (direction == 1) ? currentPlayer->getNext()
-                                              : currentPlayer->getPrev();
-
-      Player &nextPlayer = target->getData();
-
-      cout << nextPlayer.getName() << " draws 4 cards and is skipped!" << endl;
-      for (int i = 0; i < 4; i++) {
-        nextPlayer.addToHand(safeDraw());
-      }
+    if (playedCard.type == SKIP) {
       skipNextPlayer();
       return true;
     }
+
+    if (playedCard.type == DRAW_TWO) {
+      Node<Player> *target = (direction == 1) ? currentPlayer->getNext()
+                                              : currentPlayer->getPrev();
+
+      target->getData().addToHand(safeDraw());
+      target->getData().addToHand(safeDraw());
+      skipNextPlayer();
+      return true;
+    }
+
+    if (playedCard.type == WILD_COLOR || playedCard.type == WILD_DRAW_FOUR) {
+
+      waitingForWildColor = true;
+      pendingWildType = playedCard.type;
+      return false;
+    }
+
     return false;
   }
 
-  void printTurnHeader(Player &player) {
-    player.printHand();
-    cout << player.getName() << ", it's your turn. Top card is: "
-         << discardPile.getTopCard().toString() << " | Current color: ";
+  void chooseWildColor(CardColor color) {
+    if (!waitingForWildColor)
+      return;
 
-    switch (currentColor) {
-    case RED:
-      cout << "Red";
-      break;
-    case GREEN:
-      cout << "Green";
-      break;
-    case BLUE:
-      cout << "Blue";
-      break;
-    case YELLOW:
-      cout << "Yellow";
-      break;
-    case WILD:
-      cout << "Wild";
-      break;
+    currentColor = color;
+
+    if (pendingWildType == WILD_DRAW_FOUR) {
+      Node<Player> *target = (direction == 1) ? currentPlayer->getNext()
+                                              : currentPlayer->getPrev();
+
+      for (int i = 0; i < 4; i++) {
+        target->getData().addToHand(safeDraw());
+      }
+      skipNextPlayer();
     }
-    cout << endl;
+
+    waitingForWildColor = false;
+    turnState = TURN_FINISHED;
   }
 
-bool handleDraw(Player &currentPlayer) {
-    cout << currentPlayer.getName() << " chooses to draw a card." << endl;
+  void beginTurn() { turnState = WAITING_FOR_INPUT; }
 
-    Card drawnCard = safeDraw();
-    currentPlayer.addToHand(drawnCard);
+  bool isWaitingForInput() const { return turnState == WAITING_FOR_INPUT; }
 
-    cout << currentPlayer.getName() << " drew card: " << drawnCard.toString()
-         << endl;
+  bool isTurnFinished() const { return turnState == TURN_FINISHED; }
 
-    if (isCardValid(drawnCard)) {
-        cout << "Drawn card is playable. Do you want to play it? (y/n): ";
-        char playChoice;
-        cin >> playChoice;
-        if (playChoice == 'y' || playChoice == 'Y') {
-            Card playedCard = currentPlayer.playCard(currentPlayer.getHandSize() - 1);
-            discardPile.addCard(playedCard);
-            bool turnEndedEarly = playSpecialCard(playedCard);
-            if (turnEndedEarly) {
-                if (currentPlayer.getHandSize() == 0) isGameOver = true;
-                return true;
-            }
-        } else {
-            cout << "Keeping drawn card. Turn ends." << endl;
-            moveToNextPlayer();
-            return true;
-        }
+  void handleDraw() {
+    Player &player = currentPlayer->getData();
+    Card drawn = safeDraw();
+    player.addToHand(drawn);
+
+    if (isCardValid(drawn)) {
+      turnState = WAITING_FOR_INPUT;
     } else {
-        cout << "Drawn card cannot be played. Turn ends." << endl;
-        moveToNextPlayer();
-        return true;
+      moveToNextPlayer();
+      turnState = TURN_FINISHED;
     }
-    
-    if (currentPlayer.getHandSize() == 0) {
-        isGameOver = true;
-    } else {
-        moveToNextPlayer();
-    }
-    
-    return true;
-}
+  }
 
-  bool handlePlay(Player &currentPlayer, int choice) {
-    Card playedCard = currentPlayer.playCard(choice);
+  void advanceTurnIfNeeded() {
+    if (turnState == TURN_FINISHED && !isGameOver) {
+      beginTurn();
+    }
+  }
+
+  bool handlePlay(int cardIndex) {
+    if (turnState != WAITING_FOR_INPUT || waitingForWildColor)
+      return false;
+    Player &player = currentPlayer->getData();
+
+    Card selected = player.peekCard(cardIndex);
+
+    if (!isCardValid(selected)) {
+      return false;
+    }
+
+    if (selected.type == WILD_DRAW_FOUR && player.hasColor(currentColor)) {
+      return false;
+    }
+
+    Card playedCard = player.playCard(cardIndex);
     discardPile.addCard(playedCard);
 
-    bool turnEndedEarly = playSpecialCard(playedCard);
-    if (turnEndedEarly) {
-      if (currentPlayer.getHandSize() == 0) {
-        isGameOver = true;
-      }
+    bool skipHandled = playSpecialCard(playedCard);
+
+    if (player.getHandSize() == 0) {
+      isGameOver = true;
+      turnState = GAME_OVER;
       return true;
     }
 
-    if (currentPlayer.getHandSize() == 0) {
-      isGameOver = true;
-    } else {
+    if (!skipHandled) {
       moveToNextPlayer();
     }
 
+    turnState = TURN_FINISHED;
     return true;
   }
 
-  void playTurn() {
-    if (deck.isEmpty()) {
-      discardPile.resetIntoDeck(deck);
-      deck.shuffle();
-    }
-
-    Player &player = currentPlayer->getData();
-
-    printTurnHeader(currentPlayer->getData());
-
-    int choice;
-    bool validPlay = false;
-    bool wantsToDraw = false;
-    Card selectedCard;
-
-    while (!validPlay) {
-      cout << "Enter the index of the card to play: ";
-      cin >> choice;
-
-      if (choice == -1) {
-        wantsToDraw = true;
-        break;
-      }
-
-      if (choice < 0 || choice >= currentPlayer->getData().getHandSize()) {
-        cout << "Invalid index. Try again." << endl;
-        continue;
-      }
-
-      selectedCard = currentPlayer->getData().peekCard(choice);
-      validPlay = isCardValid(selectedCard);
-
-      if (validPlay && selectedCard.type == WILD_DRAW_FOUR &&
-          currentPlayer->getData().hasColor(currentColor)) {
-
-        cout << "You cannot play Wild Draw Four while holding a "
-             << (currentColor == RED     ? "Red"
-                 : currentColor == GREEN ? "Green"
-                 : currentColor == BLUE  ? "Blue"
-                                         : "Yellow")
-             << " card. Choose a different card." << endl;
-
-        validPlay = false;
-      }
-    }
-
-    if (wantsToDraw) {
-      handleDraw(currentPlayer->getData());
-
-      return;
-    }
-
-    handlePlay(currentPlayer->getData(), choice);
-  }
-
   bool gameOver() const { return isGameOver; }
+};
 
-  void run() {
-    start();
-
-    cout << ">>>>>  UNO  <<<<<" << endl;
-    cout << "Each player has been dealt 7 cards." << endl;
-
-    while (!gameOver()) {
-      playTurn();
-    }
-
-    for (int i = 0; i < totalPlayers; i++) {
-      if (getPlayerHandSize(i) == 0) {
-        cout << getPlayerByIndex(i).getName() << " wins the game!" << endl;
-        break;
-      }
-    }
-
-    cout << "Thanks for playing UNO!" << endl;
+class UnoGUI {
+public:
+  int screenWidth = 1440;
+  int screenHeight = 900;
+  void init() {
+    InitWindow(screenWidth, screenHeight, "UNO - LinkedList Implementation");
+    SetTargetFPS(60);
   }
+
+  void shutdown() { CloseWindow(); }
+
+  void beginFrame() {
+    BeginDrawing();
+    ClearBackground(DARKGREEN);
+  }
+
+  void endFrame() { EndDrawing(); }
 };
 
 int main() {
   Game game;
-  game.run();
+  UnoGUI gui;
+
+  gui.init();
+
+  game.start();
+  game.beginTurn();
+
+  int turnCounter = 0;
+
+  while (!WindowShouldClose()) {
+    gui.beginFrame();
+
+    DrawText("UNO Game", 20, 20, 30, WHITE);
+
+    if (!game.gameOver()) {
+      int currentPlayerId = game.getCurrentPlayerId();
+      Player &currentPlayer = game.getCurrentPlayer();
+      string playerName = currentPlayer.getName();
+
+      DrawText(("Current Player: " + playerName).c_str(), 20, 60, 20, WHITE);
+
+      Card topCard = game.getTopCard();
+      string topCardText = "Top Card: " + topCard.toString();
+      DrawText(topCardText.c_str(), 20, 90, 20, WHITE);
+
+      string colorText = "Current Color: ";
+      switch (game.getCurrentColor()) {
+      case CARD_RED:
+        colorText += "Red";
+        break;
+      case CARD_GREEN:
+        colorText += "Green";
+        break;
+      case CARD_BLUE:
+        colorText += "Blue";
+        break;
+      case CARD_YELLOW:
+        colorText += "Yellow";
+        break;
+      case WILD:
+        colorText += "Wild";
+        break;
+      }
+      DrawText(colorText.c_str(), 20, 120, 20, WHITE);
+
+      int handSize = currentPlayer.getHandSize();
+      DrawText(("Hand Size: " + to_string(handSize)).c_str(), 20, 150, 20,
+               WHITE);
+
+      DrawText("Your Cards:", 20, 190, 25, YELLOW);
+
+      int startY = 220;
+      for (int i = 0; i < handSize; i++) {
+        Card card = currentPlayer.peekCard(i);
+
+        Color textColor = WHITE;
+        switch (card.color) {
+        case CARD_RED:
+          textColor = RED;
+          break;
+        case CARD_GREEN:
+          textColor = GREEN;
+          break;
+        case CARD_BLUE:
+          textColor = BLUE;
+          break;
+        case CARD_YELLOW:
+          textColor = YELLOW;
+          break;
+        case WILD:
+          textColor = PURPLE;
+          break;
+        }
+
+        string cardText = "[" + to_string(i) + "] " + card.toString();
+        DrawText(cardText.c_str(), 40, startY + (i * 25), 20, textColor);
+
+        DrawRectangle(20, startY + (i * 25), 15, 20, textColor);
+        DrawRectangleLines(20, startY + (i * 25), 15, 20, WHITE);
+      }
+
+      DrawText("Controls:", 500, 200, 25, YELLOW);
+      DrawText("D - Draw a card", 520, 230, 20, WHITE);
+      DrawText("0-9 - Play card at that index", 520, 260, 20, WHITE);
+      DrawText("R/G/B/Y - Choose color for Wild card", 520, 290, 20, WHITE);
+      DrawText("ESC - Exit", 520, 320, 20, WHITE);
+
+      if (game.isWaitingForInput()) {
+        DrawText("Status: Waiting for your move", 520, 350, 20, GREEN);
+      } else if (game.isTurnFinished()) {
+        DrawText("Status: Turn finished", 520, 350, 20, ORANGE);
+      }
+
+      if (game.isWaitingForInput()) {
+        for (int i = KEY_ZERO; i <= KEY_NINE; i++) {
+          if (IsKeyPressed(i)) {
+            int cardIndex = i - KEY_ZERO;
+            cout << playerName << " attempting to play card at index "
+                 << cardIndex << endl;
+
+            if (cardIndex < handSize) {
+              if (game.handlePlay(cardIndex)) {
+                cout << "Card played successfully!" << endl;
+              } else {
+                cout << "Invalid move!" << endl;
+              }
+            } else {
+              cout << "Invalid card index!" << endl;
+            }
+            break;
+          }
+        }
+
+        if (IsKeyPressed(KEY_D)) {
+          cout << playerName << " drawing a card..." << endl;
+          game.handleDraw();
+        }
+
+        if (IsKeyPressed(KEY_R)) {
+          cout << playerName << " chose RED for wild card" << endl;
+          game.chooseWildColor(CARD_RED);
+        }
+        if (IsKeyPressed(KEY_G)) {
+          cout << playerName << " chose GREEN for wild card" << endl;
+          game.chooseWildColor(CARD_GREEN);
+        }
+        if (IsKeyPressed(KEY_B)) {
+          cout << playerName << " chose BLUE for wild card" << endl;
+          game.chooseWildColor(CARD_BLUE);
+        }
+        if (IsKeyPressed(KEY_Y)) {
+          cout << playerName << " chose YELLOW for wild card" << endl;
+          game.chooseWildColor(CARD_YELLOW);
+        }
+      }
+
+      game.advanceTurnIfNeeded();
+
+      turnCounter++;
+    } else {
+      int screenWidth = 1440;
+      int screenHeight = 900;
+      DrawText("GAME OVER!", screenWidth / 2 - 100, screenHeight / 2 - 50, 40,
+               RED);
+
+      Player &currentPlayer = game.getCurrentPlayer();
+      if (currentPlayer.getHandSize() == 0) {
+        DrawText(("Winner: " + currentPlayer.getName() + "!").c_str(),
+                 screenWidth / 2 - 100, screenHeight / 2, 30, GREEN);
+      }
+
+      DrawText("Press ESC to exit", screenWidth / 2 - 100,
+               screenHeight / 2 + 50, 30, WHITE);
+
+      if (IsKeyPressed(KEY_ESCAPE)) {
+        break;
+      }
+    }
+
+    gui.endFrame();
+  }
+
+  cout << "Game ended after " << turnCounter << " turns" << endl;
+  gui.shutdown();
+
   return 0;
 }
