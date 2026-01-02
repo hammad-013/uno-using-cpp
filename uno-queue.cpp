8c2d55645b03ed7601f3d7ec9d9b19e2f00d28cf
junaid-gondal <gondalj646@gmail.com>
2026-01-02 17:29:50 +0500

    ds to war gai guru

--- uno-queue.cpp
+++ uno-queue.cpp
@@ -1,9 +1,143 @@
 #include <iostream>
-#include <queue>
 #include <string>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+
+template <typename T>
+class Queue
+{
+private:
+    struct Node
+    {
+        T data;
+        Node* next;
+    };
+
+    Node* frontNode;
+    Node* rearNode;
+    int counts;
+
+public:
+    // Constructor
+    Queue()
+    {
+        frontNode = nullptr;
+        rearNode = nullptr;
+        counts = 0;
+    }
+    // Copy Constructor
+Queue(const Queue& other)
+{
+    frontNode = rearNode = nullptr;
+    counts = 0;
+
+    Node* curr = other.frontNode;
+    while (curr != nullptr)
+    {
+        push(curr->data);
+        curr = curr->next;
+    }
+}
+
+// Assignment Operator
+Queue& operator=(const Queue& other)
+{
+    if (this == &other)
+        return *this;
+
+    while (!emptys())
+        pop();
+
+    Node* curr = other.frontNode;
+    while (curr != nullptr)
+    {
+        push(curr->data);
+        curr = curr->next;
+    }
+
+    return *this;
+}
+
+
+    // Destructor
+    ~Queue()
+    {
+        while (!emptys())
+            pop();
+    }
+
+    // push(): insert at rear
+    void push(const T& value)
+    {
+        Node* newNode = new Node;
+        newNode->data = value;
+        newNode->next = nullptr;
+
+        if (emptys())
+        {
+            frontNode = rearNode = newNode;
+        }
+        else
+        {
+            rearNode->next = newNode;
+            rearNode = newNode;
+        }
+        counts++;
+    }
+
+    // pop(): remove from front
+    void pop()
+    {
+        if (emptys())
+        {
+            cout << "Queue is empty. Cannot pop.\n";
+            return;
+        }
+
+        Node* temp = frontNode;
+        frontNode = frontNode->next;
+        delete temp;
+        counts--;
+
+        if (frontNode == nullptr)
+            rearNode = nullptr;
+    }
+
+    // front(): access front element
+    T fronts()
+    {
+        if (emptys())
+        {
+            cout << "Queue is empty. No front element.\n";
+            return T();   // return default value
+        }
+        return frontNode->data;
+    }
+
+    // back(): access last element
+    T backs()
+    {
+        if (emptys())
+        {
+            cout << "Queue is empty. No back element.\n";
+            return T();   // return default value
+        }
+        return rearNode->data;
+    }
+
+    // empty(): check if queue is empty
+    bool emptys() const
+    {
+        return frontNode == nullptr;
+    }
+
+    // size(): number of elements
+    int sizes() const
+    {
+        return counts;
+    }
+};
 
 struct Card
  {
@@ -12,7 +146,7 @@ struct Card
 };
 
 
-void initializeDeck(queue<Card> &deck)
+void initializeDeck(Queue<Card> &deck)
 {
     string colors[4] = {"Red", "Blue", "Green", "Yellow"};
     for (int i = 0; i < 4; i++)
@@ -23,7 +157,7 @@ void initializeDeck(queue<Card> &deck)
         {
             deck.push({c, to_string(n)});
             deck.push({c, to_string(n)});
-  }
+        }
         deck.push({c, "Skip"});
         deck.push({c, "Skip"});
         deck.push({c, "Reverse"});
@@ -39,22 +173,23 @@ void initializeDeck(queue<Card> &deck)
 }
 
 
-Card drawCard(queue<Card> &deck)
+Card drawCard(Queue<Card> &deck)
  {
-    Card c = deck.front();
+    Card c = deck.fronts();
     deck.pop();
     return c;
 }
 
-void shuffleDeck(queue<Card> &deck)
+
+void shuffleDeck(Queue<Card> &deck)
 {
     Card tempArray[108];
     int cont = 0;
 
 
-    while (!deck.empty())
+    while (!deck.emptys())
     {
-        tempArray[cont] = deck.front();
+        tempArray[cont] = deck.fronts();
         deck.pop();
         cont++;
     }
@@ -77,7 +212,10 @@ void shuffleDeck(queue<Card> &deck)
         deck.push(tempArray[i]);
     }
 }
-void dealInitialCards(queue<Card> players[], int numPlayers, queue<Card> &deck)
+
+
+
+void dealInitialCards(Queue<Card> players[], int numPlayers, Queue<Card> &deck)
  {
     for (int r = 0; r < 7; r++)
 
@@ -90,13 +228,15 @@ void dealInitialCards(queue<Card> players[], int numPlayers, queue<Card> &deck)
         }
     }
 }
-void showPlayerHand(queue<Card> &hand)
+
+
+void showPlayerHand(Queue<Card> &hand)
  {
-    int n = hand.size();
+    int n = hand.sizes();
     cout << "Hand (" << n << "): ";
     for (int i = 0; i < n; i++)
         {
-        Card c = hand.front(); hand.pop();
+        Card c = hand.fronts(); hand.pop();
         cout << "[" << i << "] " << c.color << " " << c.value;
         if (i + 1 < n) cout << " | ";
         hand.push(c);
@@ -128,7 +268,7 @@ string chooseWildColor()
     {
         return "Green";
         }
- else
+    else
     return "Yellow";
 }
 
@@ -149,12 +289,13 @@ bool isValidMove(Card &played, Card &top, string forcedColor)
         }
     return false;
 }
-void winnercondition(queue<Card> players[], int totalplayer, queue<Card> &deck)
+
+void winnercondition(Queue<Card> players[], int totalplayer, Queue<Card> &deck)
 {
     bool found=false;
     for(int i=0;i<totalplayer;i++)
         {
-        if(players[i].empty())
+        if(players[i].emptys())
         {
             cout<<"Congratulations! Player "<<i+1<<" wins the match!"<<endl;
             found=true;
@@ -163,20 +304,20 @@ void winnercondition(queue<Card> players[], int totalplayer, queue<Card> &deck)
     if(found)
      exit(0);
 
-    if(deck.empty())
+    if(deck.emptys())
         {
-        int minCards=players[0].size();
+        int minCards=players[0].sizes();
     int winner=0;
      bool tie=false;
         for(int i=1;i<totalplayer;i++)
             {
-            if(players[i].size()<minCards)
+            if(players[i].sizes()<minCards)
             {
-                minCards=players[i].size();
+                minCards=players[i].sizes();
           winner=i;
          tie=false;
          }
-            else if(players[i].size()==minCards)
+            else if(players[i].sizes()==minCards)
 
         tie=true;
         }
@@ -194,8 +335,8 @@ void winnercondition(queue<Card> players[], int totalplayer, queue<Card> &deck)
 void playGame()
  {
     int totalplayer = 2;
-    queue<Card> deck, discard;
-    queue<Card> players[totalplayer];
+    Queue<Card> deck, discard;
+    Queue<Card> players[totalplayer];
 
     initializeDeck(deck);
     shuffleDeck(deck);
@@ -221,23 +362,23 @@ void playGame()
 
     int cur = 0;
     string forcedColor = "";
-
+    int direction = 1;
 
     while (true)
         {
+        winnercondition(players, totalplayer, deck);
 
-         winnercondition(players, totalplayer, deck);
         cout << "Player " << cur + 1 << " turn:" << endl;
         showPlayerHand(players[cur]);
 
-        Card top = discard.back();
+        Card top = discard.backs();
         if (forcedColor != "")
         {
             cout << "Forced color: " << forcedColor << endl;
         }
         cout << "Top discard: " << top.color << " " << top.value << endl;
 
-        int handSize = players[cur].size();
+        int handSize = players[cur].sizes();
         cout << "Choose card and -1 to draw): ";
         int choice;
         cin >> choice;
@@ -246,7 +387,7 @@ void playGame()
 
         if (choice == -1)
             {
-              if (deck.empty())
+              if (deck.emptys())
             {
                 cout << "Deck empty, cannot draw"<<endl;
 
@@ -272,18 +413,44 @@ void playGame()
 
             showPlayerHand(players[cur]);
 
+
+            cout << "Choose a card to play from your hand (-1 to skip turn): ";
+            cin >> choice;
+
+            if (choice == -1)
+            {
+               if(cur==0)
+                {
+                    cur++;
+
+                }
+
+              else  if(cur==1)
+                {
+                    cur --;
+
+
+                }
+                continue;
+
+            }
+
+            handSize = players[cur].sizes();
         }
-           if (choice < 0 || choice >= handSize)
+
+
+        if (choice < 0 || choice >= handSize)
             {
             cout << "Invalid choice"<<endl;
             continue;
         }
 
-        queue<Card> tmp;
+
+        Queue<Card> tmp;
         Card play;
         for (int i = 0; i < handSize; i++)
         {
-            Card c = players[cur].front();
+            Card c = players[cur].fronts();
             players[cur].pop();
             if (i == choice)
             {
@@ -297,7 +464,7 @@ void playGame()
 
         if (!isValidMove(play, top, forcedColor))
             {
-            cout << "Invalid move. You cannot play this card now."<<endl;
+            cout << "Invalid move. You cannot play this card now.\n";
             players[cur].push(play);
                 if(cur==0)
                 {
@@ -318,6 +485,29 @@ void playGame()
 
         cout << "Player " << cur + 1 << " played: " << play.color << " " << play.value << endl;
 
+          if (players[cur].sizes() == 1)
+        {
+            cout << "Say UNO! (1 = YES, 0 = NO): ";
+            int sayUno;
+            cin >> sayUno;
+            cout << endl;
+
+            if (sayUno != 1)
+            {
+                cout << "You forgot to say UNO! Penalty: Draw 4 cards." << endl;
+                for (int i = 0; i < 4; i++)
+                {
+                    if (!deck.emptys())
+                        {
+                            players[cur].push(drawCard(deck));
+                        }
+                }
+            }
+            else
+            {
+                cout << "UNO!" << endl;
+            }
+        }
         if (play.value == "Wild" || play.value == "+4")
         {
 
@@ -329,7 +519,35 @@ void playGame()
            }
 
 
+        if (play.value == "+4")
+            { int next;
+                if(cur==0)
+                {
+                    next= cur +1;
+                }
+              else if(cur==1)
+                {
+                    next=cur - 1;
 
+                }
+
+            cout << "Next player draws 4 cards and also his turn got skipped."<<endl;
+            for (int i = 0; i < 4; i++)
+               {
+
+               if (deck.emptys())
+               {
+                   break;
+                   }
+                else
+                {
+                    players[next].push(drawCard(deck));
+
+                    }
+                }
+
+            continue;
+        }
         if(top.value=="Draw2" && top.color==play.color&& play.value=="Draw2")
         {
 
@@ -351,6 +569,18 @@ void playGame()
 
         }
 
+        if (play.value == "Skip")
+            {
+            cout << "Next player got skipped" << endl;
+
+            continue;
+        }
+        if (play.value == "Reverse")
+            {
+            cout << " game reversed so again " <<cur+1  <<" player's turn " << endl;
+
+            continue;
+        }
          else if (play.value == "Draw2")
          {   int next;
                 if(cur==0)
@@ -368,7 +598,7 @@ void playGame()
             cout << "Next player draws 2 cards. and also skipped" << endl;
             for (int i = 0; i < 2; i++)
                 {
-                    if (deck.empty())
+                    if (deck.emptys())
                     {
                       break;
                     }
@@ -380,6 +610,109 @@ void playGame()
                 }
             continue;
         }
+
+  if (play.color == "Wild" && play.value == "Wild")
+    {
+
+    Card topBeforeWild = top;
+    discard.push(play);
+
+    cout << "Special Wild Wild rule activated!"<<endl;
+    int ch;
+    cout << "If you want to see Player " << cur + 1 << "'s hand then enter 1, else 2: ";
+    cin >> ch;
+    cout<<endl;
+    if (ch == 2)
+    {    if(cur==0)
+       {
+          cur++;
+
+        }
+      else  if(cur==1)
+        {
+          cur --;
+
+
+        }
+        continue;
+
+    }
+    int nextPlayer;
+         if(cur==0)
+       {
+           nextPlayer=cur +1;
+
+                }
+      else if(cur==1)
+     {
+        nextPlayer = cur -1;
+
+
+    }
+
+
+    cout << "Player " << nextPlayer + 1 << " can see Player " << cur + 1 << "'s hand:"<<endl;
+    showPlayerHand(players[cur]);
+
+
+  bool found = false;
+Queue<Card> tmpHand;
+
+while (!players[cur].emptys())
+{
+    Card c = players[cur].fronts();
+    players[cur].pop();
+
+    if (c.color == topBeforeWild.color)
+     {
+        found = true;
+    }
+
+    tmpHand.push(c);
+}
+
+
+players[cur] = tmpHand;
+
+
+    if (found)
+    {
+        cout << "Player " << nextPlayer + 1 << " matched the color!"<<endl;
+
+        cout << "Player " << cur + 1 << " (who played Wild Wild) draws 4 cards."<<endl;
+
+        for (int i = 0; i < 4; i++)
+        {
+           if (!deck.emptys()) players[cur].push(drawCard(deck));
+        }
+
+    }
+    else
+     {
+        cout << "Player " << nextPlayer + 1 << " could not match the color "<<endl;
+        cout << "Player " << nextPlayer + 1 << " draws 6 cards "<<endl;
+        for (int i = 0; i < 6; i++)
+        {
+             if (!deck.emptys())
+             {
+                 players[nextPlayer].push(drawCard(deck));
+             }
+        }
+
+    }
+       if(cur==0)
+       {
+          cur++;
+
+        }
+      else  if(cur==1)
+        {
+          cur --;
+
+
+        }
+        continue;
+           }
 
        else
         {  if(cur==0)
@@ -404,6 +737,3 @@ int main()
     playGame();
     return 0;
 }
-
-
-
