f8ac50341b354515b27800a2c4fa88fcf42f81be
Momina Umar <m61963193@gmail.com>
2026-01-04 03:35:32 -0800

    Implement getCardValue function

--- uno-queue.cpp
+++ uno-queue.cpp
@@ -1,157 +1,40 @@
 #include <iostream>
-#include <queue>
 #include <string>
-#include <cstdlib>
-#include <ctime>
 using namespace std;
 
-struct Card
- {
-    string color;
-    string value;
-};
 
+class Card {
+public:
+    string name;
 
-void initializeDeck(queue<Card> &deck)
-{
-    string colors[4] = {"Red", "Blue", "Green", "Yellow"};
-    for (int i = 0; i < 4; i++)
-        {
-        string c = colors[i];
-        deck.push({c, "0"});
-        for (int n = 1; n <= 9; n++)
-        {
-            deck.push({c, to_string(n)});
-            deck.push({c, to_string(n)});
-  }
-        deck.push({c, "Skip"});
-        deck.push({c, "Skip"});
-        deck.push({c, "Reverse"});
-        deck.push({c, "Reverse"});
-        deck.push({c, "Draw2"});
-        deck.push({c, "Draw2"});
-    }
-    for (int i = 0; i < 4; i++)
-    {
-        deck.push({"Wild", "Wild"});
-        deck.push({"Wild", "+4"});
-    }
-}
+    Card() { name = ""; }
+    Card(string n) { name = n; }
 
+    string getColor() const {
+        if (name == "WILD" || name == "WILD_DRAW4") return "WILD";
 
-Card drawCard(queue<Card> &deck)
- {
-    Card c = deck.front();
-    deck.pop();
-    return c;
-}
+        int pos = 0;
+        while (pos < name.length() && name[pos] != '_') pos++;
+        if (pos >= name.length()) return "";
 
-void shuffleDeck(queue<Card> &deck)
-{
-    Card tempArray[108];
-    int cont = 0;
-
-
-    while (!deck.empty())
-    {
-        tempArray[cont] = deck.front();
-        deck.pop();
-        cont++;
-    }
-
-    srand(time(NULL));
-
-
-    for (int i = 0; i < cont; i++)
-    {
-        int r = rand() % cont;
-
-        Card temp = tempArray[i];
-        tempArray[i] = tempArray[r];
-        tempArray[r] = temp;
-    }
-
-
-    for (int i = 0; i < cont; i++)
-    {
-        deck.push(tempArray[i]);
-    }
-}
-void dealInitialCards(queue<Card> players[], int numPlayers, queue<Card> &deck)
- {
-    for (int r = 0; r < 7; r++)
-
-    {
-
-     for (int p = 0; p < numPlayers; p++)
-           {
-
-            players[p].push(drawCard(deck));
+        string color = "";
+        for (int i = 0; i < pos; i++) {
+            color += name[i];
         }
+        return color;
     }
-}
-void showPlayerHand(queue<Card> &hand)
- {
-    int n = hand.size();
-    cout << "Hand (" << n << "): ";
-    for (int i = 0; i < n; i++)
-        {
-        Card c = hand.front(); hand.pop();
-        cout << "[" << i << "] " << c.color << " " << c.value;
-        if (i + 1 < n) cout << " | ";
-        hand.push(c);
+ string getValue() const {
+        if (name == "WILD_NORMAL") return "WILD";
+        if (name == "WILD_DRAW_FOUR") return "DRAW_FOUR";
+
+        int pos = 0;
+        while (pos < name.length() && name[pos] != '_') pos++;
+        if (pos >= name.length()) return name;
+
+        pos++;
+        string value = "";
+        for (int i = pos; i < name.length(); i++) {
+            value += name[i];
+        }
+        return value;
     }
-    cout << endl;
-}
-
-
-string chooseWildColor()
-{
-    string col;
-    while (true)
-        {
-        cout << "Choose a color (R/B/G/Y): ";
-        cin >> col;
-        if (col == "R" || col == "B" || col == "G" || col == "Y"|| col == "r" || col == "b" || col == "g" || col == "y")
-            break;
-        cout << "wrong choice again enter correct "<<endl;
-    }
-    if(col=="r"||col=="R")
-    {
-        return "Red";
-        }
-    if(col=="b"||col=="B")
-    {
-        return "Blue";
-        }
-    if(col=="g"||col=="G")
-    {
-        return "Green";
-        }
- else
-    return "Yellow";
-}
-
-
-bool isValidMove(Card &played, Card &top, string forcedColor)
- {
-    if (played.value=="Wild" || played.value=="+4")
-    {
-        return true;
-        }
-    if (forcedColor!="" && played.color==forcedColor)
-    {
-        return true;
-        }
-    if (forcedColor=="" && (played.color==top.color || played.value==top.value))
-    {
-        return true;
-        }
-    return false;
-}
-
-
-
-
-
-
